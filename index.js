7eb234dca9b57f3bf69b804a411ba1a851953535
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-06-09 12:48:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#searchLink").click(showRepositories);
-    
+
 	function showRepositories()
     {
       if($("#username").val().length === 0){
@@ -15,36 +15,36 @@ $(document).ready(function() {
 			 content += "<div class='card border-primary mb-3'><img class='card-img-top' src="+data[i].owner.avatar_url+"><div class='card-body'><h5 class='card-title'>"+data[i].name+"</h5><p class='card-text'>"
 			 +data[i].description+"</p><a href="+data[i].html_url+" class='card-link'>"+data[i].name+"</a><p>Login: "
 			 +data[i].owner.login+"</p><a href="+data[i].owner.html_url+" class='card-link'>"
-			 +data[i].owner.html_url+"</a><br><a href='#' class='card-link showCom' data-repository="+data[i].name+" data-username="+data[i].owner.login+">Show Commits</button></div></div>";  
+			 +data[i].owner.html_url+"</a><br><a href='#' class='card-link showCom' data-repository="+data[i].name+" data-username="+data[i].owner.login+">Show Commits</button></div></div>";
 		   }
 		   $("#results").append(content);
 		   $(".showCom").click(showCommits);
         }).fail(displayError);
       }
     }
-		
+
     function displayError(error)
     {
       $("#errors").append("I'm sorry, there's been an error. Please try again.");
-    }	
+    }
 
 	function showCommits()
 	{
 		var el = $(this);
 		var username = el.attr('data-username');
 		var repository = el.attr('data-repository');
-		
+
 		$("#details").empty();
-		
+
 		$.get('https://api.github.com/repos/'+username+'/'+repository+'/commits',function(data){
 			var content = "";
 		   for(var i = 0; i < data.length; i++)
 		   {
 			 content += "<div class='card border-primary mb-3'><img class='card-img-top' src="+data[i].author.avatar_url+"><div class='card-body'><h5 class='card-title'>Login: "+data[i].author.login+"</h5><p class='card-text'>Name: "
-			 +data[i].commit.author.name+"</p><p class='card-text'>SHA: "+data[i].sha+"</p></div></div>";  
+			 +data[i].commit.author.name+"</p><p class='card-text'>SHA: "+data[i].sha+"</p></div></div>";
 		   }
 			$("#details").append(content);
-		}).fail(displayError);	
+		}).fail(displayError);
 	}
 });
 //octocat
